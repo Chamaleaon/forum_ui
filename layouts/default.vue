c5196f8a37420fa51dbba5926fc62e6823f6f098
chenyanfei <15873624283@163.com>
2022-02-24 22:54:26 +0800

    引入vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <h1>导航拦</h1>
-    <nuxt />
-  </div>
+  <v-app>
+    <div class="overflow-hidden">
+      <header class="fixed w-screen h-16 bg-gray-700">nav</header>
+      <div class="min-h-screen mt-16 bg-gray-300">
+        <div class="md:w-1/2 mx-auto p-2">
+          <nuxt />
+        </div>
+      </div>
+    </div>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style scoped>
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div v-for="i in 20" :key="i" class="p-2 mb-2 rounded bg-purple-500">
+      {{ i }}. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+      officiis nesciunt, ut provident, accusantium ad natus ullam reprehenderit
+      molestias suscipit explicabo, soluta minus necessitatibus. Ratione
+      corporis inventore maxime incidunt dolore?
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
