7dc03baa3541c925b8faa5a0cda875fd4bc3e3e8
chenyanfei <15873624283@163.com>
2022-02-25 22:52:28 +0800

    deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'forum_ui',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
